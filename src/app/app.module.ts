bee0119fa96a1582dbf2d6221a0fe5689fa2a5b2
Hung Tran <hungtran891989@gmail.com>
2018-12-02 09:57:51 +0700

    installed ngrx dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { JediModule } from './jedi/jedi.module';
@@ -13,6 +14,9 @@ import { CounterModule } from './counter/counter.module';
   imports: [
     BrowserModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25 // retain the last 25 states
+    }),
     CounterModule,
     JediModule
   ],
