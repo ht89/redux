b0024cda938183f7dfebd460f84d15ec87d8644b
Hung Tran <hungtran891989@gmail.com>
2018-12-02 16:43:07 +0700

    commit

--- src/app/app-state.ts
+++ src/app/app-state.ts
@@ -1,11 +1,12 @@
-import { Jedi } from './jedi/jedi-list/jedi.model';
 import { FeatureProducts } from './product/product.reducer';
 import { JediListState } from './jedi/jedi-list/jedi-list.reducer';
 import { CounterState } from './counter/counter.module';
+import { State } from './app.module';
 
 // typed representation of the store
 export interface AppState {
   counter: CounterState;
   jediList: JediListState;
   featureProducts: FeatureProducts;
+  users: State;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from './app-state';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -18,9 +19,36 @@ import { AppState } from './app-state';
     <br />
 
     <app-products></app-products>
+
+    <div style="border: solid 1px black; padding: 10px;"
+      *ngFor="let user of users$ | async">
+      {{ user.name }}
+    </div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  users$;
 
+  constructor(private store: Store<AppState>) {
+    this.users$ = this.store
+      .select(state => state.users.entities)
+      .pipe(
+        map(this.toArray)
+      );
+
+    this.users$
+      .subscribe(data => console.log(data));
+  }
+
+  toArray(obj) {
+
+    if (!obj) {
+      return obj;
+    }
+
+    const keys = Object.keys(obj);
+    keys.map(key => obj[key]);
+
+  }
 }
